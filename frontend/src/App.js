44c12f3dc227f8e923cdd40ac2ecf785ecc53f68
Liya Prince <liyaprince2027@cs.sjcetpalai.ac.in>
2026-02-22 17:33:04 +0530

    added printrequest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,21 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Printout from "./pages/Printout";
+import SubmitPrintRequest from "./pages/SubmitPrintRequest";
 import "./App.css";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/login/:role" element={<Login />} />
-        <Route path="/register/:role" element={<Register />} />
-        <Route path="/:role/dashboard" element={<Dashboard />} />
-        <Route path="/:role/printout" element={<Printout />} />
-      </Routes>
-    </Router>
+  return ( 
+  <Router>
+     <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/login/:role" element={<Login />} />
+      <Route path="/register/:role" element={<Register />} />
+      <Route path="/:role/dashboard" element={<Dashboard />} />
+      <Route path="/:role/printout" element={<Printout />} /> 
+      <Route path="/:role/printout/submit" element={<SubmitPrintRequest />} />
+     </Routes>
+  </Router>
   );
-}
-
-export default App;
+ } 
+ export default App;
